5ef8dab43acb1273f602ffbb24bd2addfa6654b4
QuantumInformatik <juanguillermoalarcon@gmail.com>
2023-03-08 23:52:00 -0500

    se agrega PersonalDataActivity con campos de nombres, apellidos, sexo y fecha de nacimiento

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20231/lab1/PersonalDataActivity.java
@@ -0,0 +1,59 @@
+package co.edu.udea.compumovil.gr03_20231.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class PersonalDataActivity extends AppCompatActivity {
+    private Date fechaSeleccionada;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_data);
+        setListeners();
+    }
+
+    public void setListeners() {
+        Button btnCambiarDatePicker = findViewById(R.id.btnCambiar);
+        btnCambiarDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+                // Crear un diálogo de DatePicker
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        PersonalDataActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                Calendar fechaCalendario = Calendar.getInstance();
+                                fechaCalendario.set(year, month, dayOfMonth);
+                                fechaSeleccionada = fechaCalendario.getTime();
+                                Log.i("MY_TAG", "Fecha seleccionada = "+fechaSeleccionada);
+
+                            }
+                        },
+                        year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        Log.i("MY_TAG", "Fecha seleccionada = "+fechaSeleccionada);
+
+
+    }
+}
