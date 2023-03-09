dfb4ebbb7985b8774d80be152649a1ad959780d8
berracode <juanguillermoalarcon@gmail.com>
2023-03-09 12:48:49 -0500

    se agrega spinner, se agrega landscape orientation

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20231/lab1/PersonalDataActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20231/lab1/PersonalDataActivity.java
@@ -6,8 +6,10 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -21,6 +23,21 @@ public class PersonalDataActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_data);
         setListeners();
+        setSpinner();
+    }
+
+    public void setSpinner(){
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerGradoEscolaridad);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.grados_escolaridad, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        String elementoSeleccionado = spinner.getSelectedItem().toString();
+
     }
 
     public void setListeners() {
